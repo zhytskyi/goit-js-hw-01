fb737ef47904eef129928dce001aa96a86543f9b
Denis <denzhytskyi@icloud.com>
2024-06-11 23:20:19 +0200

    New js-files upload

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  return (
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) +
+    Number.parseFloat(padding) +
+    Number.parseFloat(border) +
+    Number.parseFloat(border)
+  );
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
